d85f61a51b9305d8a4913369299fb8d8184f0bfc
echzny <echzny@gmail.com>
2023-07-12 13:31:41 +0900

    finish section7.1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { ErrorBoundary } from "react-error-boundary"
 import { MessageForm } from './MessageForm'
 import { Messages } from './Messages'
 
 export const App = () => {
+  // レンダリング時にMessagesがエラーになるので、ErrorBoundaryでエラーハンドリングする
   return (
     <div>
       <div>Sample Chat App</div>
       <div>
-        <Messages />
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+          <Messages />
+        </ErrorBoundary>
         <MessageForm />
       </div>
     </div>
